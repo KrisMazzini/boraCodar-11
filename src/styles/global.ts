c96188bfd8a8db547109fff8f445781d2055a8d4
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2023-03-21 20:10:36 -0300

    config fonts and theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,20 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+    :root {
+        font-size: 62.5%;
+    }
+
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+    }
+
+    body, input, button {
+        font-family: 'Titillium Web', sans-serif;
+        font-weight: 400;
+        font-size: 1.6rem;
+        line-height: 2.4rem;
     }
 `
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,4 +1,16 @@
 export const defaultTheme = {
   black: '#000000',
+  error: '#ED3A5A',
+  'gray-50': '#F8FAFC',
+  'gray-100': '#F1F5F9',
+  'gray-200': '#E2E8F0',
+  'gray-300': '#CBD5E1',
+  'gray-400': '#94A3B8',
+  'gray-500': '#64748B',
+  'gray-600': '#475569',
+  'gray-700': '#334155',
+  'gray-800': '#1E293B',
+  'gray-900': '#0F172A',
+  purple: '#7C3AED',
   white: '#FFFFFF',
 }
